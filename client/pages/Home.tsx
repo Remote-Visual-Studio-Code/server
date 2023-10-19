657bd2d3a3c5ec0a6283faac568da44b819eb844
Exortions <75327059+Exortions@users.noreply.github.com>
2023-10-18 19:53:30 -0700

    replace hello world with editor button + redirect

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Button from '@mui/material/Button';
 
 const Home: React.FC = () => {
-    return <Button variant="contained">Hello World</Button>;
+    return (
+        <Button variant="contained" onClick={() => (window.location.href = '/editor')}>
+            Editor
+        </Button>
+    );
 };
 
 export default Home;
